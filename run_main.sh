f589aae10d456d0b8f9d54e4b69d4b2640024550
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-01-09 12:45:54 -0500

    boot script

--- run_main.sh
+++ run_main.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Wait for 30 seconds to allow other services to initialize
+sleep 30
 
+# Navigate to the project directory
+cd /ricochet-dev || exit
+
+# Pull the latest changes from the GitHub repository
+git pull origin main
+
+# Run the main.py script
+python3 main.py
